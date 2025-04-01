56d6a3796e5adf21f3f1f12251552ebc8e75de31
Anton Donchenko <donch.a080@gmail.com>
2025-04-01 18:59:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,52 @@
 'use strict';
 
-// write your code here
+const logoElement = document.querySelector('.logo');
+const body = document.querySelector('body');
+
+logoElement.addEventListener('click', clickHandler);
+
+function clickHandler() {
+  const promise1 = new Promise((resolve, reject) => {
+    resolve('Promise was resolved!');
+  });
+
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Promise was rejected!'));
+    }, 3000);
+  });
+
+  promise1
+    .then((message) => {
+      const divElement = document.createElement('div');
+
+      divElement.classList.add('message');
+      divElement.textContent = message;
+      body.append(divElement);
+    })
+    .catch((error) => {
+      const divElement = document.createElement('div');
+
+      divElement.classList.add('message', 'error-message');
+      divElement.textContent = error.message;
+      body.append(divElement);
+    });
+
+  promise2
+    .then((message) => {
+      const divElement = document.createElement('div');
+
+      divElement.classList.add('message');
+      divElement.textContent = message;
+      body.append(divElement);
+    })
+    .catch((error) => {
+      const divElement = document.createElement('div');
+
+      divElement.classList.add('message', 'error-message');
+      divElement.textContent = error.message;
+      body.append(divElement);
+    });
+
+  return promise1;
+}

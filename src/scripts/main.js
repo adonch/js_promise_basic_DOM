de1944b1fa84e69413ab568af04d45f0b759e932
Anton Donchenko <donch.a080@gmail.com>
2025-04-01 20:22:20 +0300

    solution fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,52 +1,39 @@
 'use strict';
 
-const logoElement = document.querySelector('.logo');
-const body = document.querySelector('body');
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
 
-logoElement.addEventListener('click', clickHandler);
-
-function clickHandler() {
-  const promise1 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', () => {
     resolve('Promise was resolved!');
   });
+});
 
-  const promise2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject(new Error('Promise was rejected!'));
-    }, 3000);
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+promise1
+  .then((result) => {
+    appendMessage(result);
+  })
+  .catch((error) => {
+    appendMessage(error.message, true);
   });
 
-  promise1
-    .then((message) => {
-      const divElement = document.createElement('div');
+promise2
+  .then((result) => {
+    appendMessage(result);
+  })
+  .catch((error) => {
+    appendMessage(error.message, true);
+  });
 
-      divElement.classList.add('message');
-      divElement.textContent = message;
-      body.append(divElement);
-    })
-    .catch((error) => {
-      const divElement = document.createElement('div');
+function appendMessage(text, isError = false) {
+  const messageDiv = document.createElement('div');
 
-      divElement.classList.add('message', 'error-message');
-      divElement.textContent = error.message;
-      body.append(divElement);
-    });
-
-  promise2
-    .then((message) => {
-      const divElement = document.createElement('div');
-
-      divElement.classList.add('message');
-      divElement.textContent = message;
-      body.append(divElement);
-    })
-    .catch((error) => {
-      const divElement = document.createElement('div');
-
-      divElement.classList.add('message', 'error-message');
-      divElement.textContent = error.message;
-      body.append(divElement);
-    });
-
-  return promise1;
+  messageDiv.className = isError ? 'message error-message' : 'message';
+  messageDiv.textContent = text;
+  document.body.appendChild(messageDiv);
 }
